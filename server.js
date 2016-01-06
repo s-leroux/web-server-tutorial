78fc4c889d586ea27170fe765c63351e18df4866
Sylvain Leroux <sylvain@chicoree.fr>
2016-01-06 16:18:43 +0100

    Added a basic logger middleware

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ var middleware = {
     requireAuthentication: function(req,res, next) {
         console.log("Private route hit!");
         next();
-    } 
+    },
+    logger: function(req,res,next) {
+        var date = new Date().toString();
+        console.log("%s %s %s", date, req.method, req.originalUrl);
+        next();
+    },
 };
 
-// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
 
 app.get('/', function(req, res) {
     res.send('Hello Express !');
